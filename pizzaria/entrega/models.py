426a5ab64e7bd4a3d547bc2f7906492b47834712
Tauan Aquino <tauanpalmeiras@hotmail.com>
2012-03-22 23:18:24 -0300

    alterações no models da entrega

--- pizzaria/entrega/models.py
+++ pizzaria/entrega/models.py
@@ -5,11 +5,18 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=128)
-    fone = models.CharField(max_length=16)
+    nome = models.CharField(max_length=128, db_index=True)
+    fone = models.CharField(max_length=16, db_index=True)
+    ramal = models.CharField(max_length=4, blank=True)
     email = models.EmailField(blank=True)
+    logradouro = models.CharField(max_length=64, db_index=True)
+    numero = models.PositiveIntegerField(verbose_name=u'nº')
+    complemento = models.CharField(max_length=32, blank=True)
+    obs = models.TextField(verbose_name=u'observação')
     
     class Meta:
+        ordering = ('nome','fone')
+        unique_together = ('fone','ramal')
         db_table = 'pedidos_cliente'
     
     def __unicode__(self):
